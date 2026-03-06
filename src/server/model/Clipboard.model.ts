4cf5d0cd4132515a07c9905dff337385198bd433
Bérenger Hto <138378950+berenger-hto@users.noreply.github.com>
2026-03-06 22:16:56 +0100

    6mars2025

--- src/server/model/Clipboard.model.ts
+++ src/server/model/Clipboard.model.ts
@@ -13,13 +13,18 @@ export class ClipboardModel {
     public async getOneData(id: string) {
         const data = await db.get({ _id: id })
         if (!data || data.length === 0) return null
+
+        return formatData(data)[0]
+        /*
         return {
             id: data[0]._id,
             type: data[0].type,
             createdAt: (new Date(data[0].createdAt).getTime()),
             source: data[0].source,
-            value: data[0].content
+            value: data[0].content,
+            
         }
+        */
     }
 
     public async deleteData(item: string | Record<string, string | number>, multiMode: boolean = false) {
